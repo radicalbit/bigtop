ddeeac361c97f69bcd256aeac5ba1d73b184b3a3
maocorte <mauro.cortellazzi@radicalbit.io>
2016-03-09 09:33:03 +0100

    cassandra RPM WIP

---
+++ bigtop-packages/src/common/cassandra/install_cassandra.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+usage() {
+  echo "
+usage: $0 <options>
+  Required not-so-options:
+     --build-dir=DIR             path to flink dist.dir
+     --prefix=PREFIX             path to install into
+  Optional options:
+     --lib-dir=DIR               path to install flink home [/usr/lib/cassandra]
+     --bin-dir=DIR               path to install bins [/usr/bin]
+     ... [ see source for more similar options ]
+  "
+  exit 1
+}
+
+OPTS=$(getopt \
+  -n $0 \
+  -o '' \
+  -l 'prefix:' \
+  -l 'var-dir:' \
+  -l 'lib-dir:' \
+  -l 'bin-dir:' \
+  -l 'build-dir:' -- "$@")
+
+if [ $? != 0 ] ; then
+    usage
+fi
+
+eval set -- "$OPTS"
+while true ; do
+    case "$1" in
+        --prefix)
+        PREFIX=$2 ; shift 2
+        ;;
+        --build-dir)
+        BUILD_DIR=$2 ; shift 2
+        ;;
+        --lib-dir)
+        LIB_DIR=$2 ; shift 2
+        ;;
+        --bin-dir)
+        BIN_DIR=$2 ; shift 2
+        ;;
+        --var-dir)
+        VAR_DIR=$2 ; shift 2
+        ;;
+        --)
+        shift ; break
+        ;;
+        *)
+        echo "Unknown option: $1"
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+for var in PREFIX BUILD_DIR ; do
+  if [ -z "$(eval "echo \$$var")" ]; then
+    echo Missing param: $var
+    usage
+  fi
+done
+
+LIB_DIR=${LIB_DIR:-/usr/lib/cassandra}
+BIN_DIR=${BIN_DIR:-/usr/bin}
+CONF_DIR=${CONF_DIR:-/etc/cassandra/conf.dist}
+
+install -d -m 0755 $PREFIX/$LIB_DIR
+install -d -m 0755 $PREFIX/$LIB_DIR/bin
+install -d -m 0755 $PREFIX/$LIB_DIR/lib
+install -d -m 0755 $PREFIX/$LIB_DIR/build
+install -d -m 0755 $PREFIX/$LIB_DIR/pylib
+install -d -m 0755 $PREFIX/$VAR_DIR/lib/cassandra
+install -d -m 0755 $PREFIX/$VAR_DIR/log/cassandra
+install -d -m 0755 $PREFIX/$VAR_DIR/run/cassandra
+
+cp -ra ${BUILD_DIR}/lib/* $PREFIX/${LIB_DIR}/lib/
+cp -a ${BUILD_DIR}/bin/* $PREFIX/${LIB_DIR}/bin
+cp -ra ${BUILD_DIR}/build/* $PREFIX/${LIB_DIR}/build/
+cp -ra ${BUILD_DIR}/pylib/* $PREFIX/${LIB_DIR}/pylib/
+
+rm -rf $PREFIX/${LIB_DIR}/bin/*.bat
+rm -rf $PREFIX/${LIB_DIR}/lib/sigar-bin/*.dll
+rm -rf $PREFIX/${LIB_DIR}/lib/sigar-bin/*.lib
+
+# Copy in the configuration files
+install -d -m 0755 $PREFIX/$CONF_DIR
+cp -a ${BUILD_DIR}/conf/* $PREFIX/$CONF_DIR
+ln -s /etc/flink/conf $PREFIX/$LIB_DIR/conf
+
+# Copy in the /usr/bin/flink wrapper
+install -d -m 0755 $PREFIX/$BIN_DIR
+cat > $PREFIX/$BIN_DIR/cassandra <<EOF
+
+!/bin/bash
+# Autodetect JAVA_HOME if not defined
+. /usr/lib/bigtop-utils/bigtop-detect-javahome
+
+# FIXME: flink-994
+#export HADOOP_HOME=\${HADOOP_HOME:-/usr/lib/hadoop}
+#export HADOOP_CONF_DIR=\${HADOOP_CONF_DIR:-/etc/hadoop/conf}
+#export FLINK_HOME=\${FLINK_HOME:-$INSTALLED_LIB_DIR}
+#export FLINK_CONF_DIR=\${FLINK_CONF_DIR:-$CONF_DIR}
+# FIXME: the following line is a workaround for BIGTOP-259
+#export HADOOP_CLASSPATH="`echo /usr/lib/flink/flink-examples-*-job.jar`":\$HADOOP_CLASSPATH
+#exec $INSTALLED_LIB_DIR/bin/flink "\$@"
+
+EOF
+chmod 755 $PREFIX/$BIN_DIR/cassandra
---
+++ bigtop-packages/src/rpm/cassandra/SPECS/cassandra.spec
@@ -0,0 +1,83 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+%define cassandra_name cassandra
+%define lib_cassandra /usr/lib/%{cassandra_name}
+%define bin_cassandra /usr/bin
+%define var_run /var/run/cassandra
+%define var_log /var/log/cassandra
+%define var_lib /var/lib/cassandra
+%define etc_cassandra /etc/%{cassandra_name}
+%define config_cassandra %{etc_cassandra}/conf
+%define man_dir %{_mandir}
+%define _binaries_in_noarch_packages_terminate_build   0
+
+%if  %{!?suse_version:1}0
+%define doc_cassandra %{_docdir}/%{cassandra_name}
+%define alternatives_cmd alternatives
+%define build_cassandra %{_builddir}/%{cassandra_name}-%{cassandra_version}/
+
+%else
+%define doc_cassandra %{_docdir}/%{cassandra_name}
+%define alternatives_cmd update-alternatives
+%endif
+
+Name: %{cassandra_name}
+Version: %{cassandra_version}
+Release: %{cassandra_release}
+Summary: Apache cassandra
+License: ASL 2.0
+URL: http://cassandra.apache.org/
+Group: Development/Libraries
+Buildroot: %{_topdir}/INSTALL/%{name}-%{version}
+BuildArch: noarch
+Source0: cassandra-%{cassandra_base_version}.zip
+Source1: do-component-build
+Source2: install_cassandra.sh
+Source3: bigtop.bom
+Requires: bigtop-utils >= 0.7
+
+%description
+Apache cassandra
+
+%prep
+%setup -n %{cassandra_name}-%{cassandra_base_version}
+
+%build
+bash $RPM_SOURCE_DIR/do-component-build
+#env cassandra_VERSION=%{cassandra_base_version} bash %{SOURCE1}
+
+%install
+%__rm -rf $RPM_BUILD_ROOT
+
+bash %{SOURCE2} \
+    --build-dir=%{build_cassandra} \
+    --bin-dir=%{_bindir} \
+    --var-dir=%{_var}  \
+    --prefix="${RPM_BUILD_ROOT}"
+
+#sh -x %{SOURCE2} --prefix=$RPM_BUILD_ROOT --doc-dir=%{doc_cassandra} --build-dir=%{build_cassandra}
+
+%post
+%{alternatives_cmd} --install %{config_cassandra} %{cassandra_name}-conf %{config_cassandra}.dist 30
+
+%files
+%defattr(-,root,root,755)
+%config(noreplace) %{config_cassandra}.dist
+%{lib_cassandra}
+%{bin_cassandra}/cassandra
+%{var_run}
+%{var_log}
+%{var_lib}
